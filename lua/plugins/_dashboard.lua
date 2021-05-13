a7b1895650408164f30cd1d35e3d9294003d957c
DumbEpsKu <63652620+EpsilonKu@users.noreply.github.com>
2021-05-14 05:22:55 +0600

    Fix logo dashboard size

--- lua/plugins/_dashboard.lua
+++ lua/plugins/_dashboard.lua
@@ -7,25 +7,8 @@ function M.config()
 	vim.g.dashboard_preview_command = 'cat'
 	vim.g.dashboard_preview_pipeline = 'lolcat --spread=2.5 -t --seed=156'
 	vim.g.dashboard_preview_file = '~/.config/nvim/logo/girl.cat'
-	vim.g.dashboard_preview_file_height = 16
-	vim.g.dashboard_preview_file_width = 80
-	-- vim.g.dashboard_custom_header = {
-	--                  "        .            .        ⣿⡇⣿⣿⣿⠛⠁⣴⣿⡿⠿⠧⠹⠿⠘⣿⣿⣿⡇⢸⡻⣿⣿⣿⣿⣿⣿⣿ ",
-	--                  "      .,;'           :,.      ⢹⡇⣿⣿⣿⠄⣞⣯⣷⣾⣿⣿⣧⡹⡆⡀⠉⢹⡌⠐⢿⣿⣿⣿⡞⣿⣿⣿ ",
-	--                  "    .,;;;,,.         ccc;.    ⣾⡇⣿⣿⡇⣾⣿⣿⣿⣿⣿⣿⣿⣿⣄⢻⣦⡀⠁⢸⡌⠻⣿⣿⣿⡽⣿⣿ ",
-	--                  "  .;c::::,,,'        ccccc:   ⡇⣿⠹⣿⡇⡟⠛⣉⠁⠉⠉⠻⡿⣿⣿⣿⣿⣿⣦⣄⡉⠂⠈⠙⢿⣿⣝⣿ ",
-	--                  "  .::cc::,,,,,.      cccccc.  ⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮ ",
-	--                  "  .cccccc;;;;;;'     llllll.    ⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀ ",
-	--                  "  .cccccc.,;;;;;;.   llllll.    ⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸ ",
-	--                  "  .cccccc  ';;;;;;'  oooooo.    ⢿⣿⡀⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡾⠁⢠⡇⢀ ",
-	--                  "  'lllllc   .;;;;;;;.oooooo'    ⢸⣿⡇⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣫⣻⡟⢀⠄⣿⣷⣾ ",
-	--                  "  'lllllc     ,::::::looooo'    ⢸⣿⡇⠄⠈⠙⠿⣿⣿⣿⣮⣿⣿⣿⣿⣿⣿⣿⣿⡿⢠⠊⢀⡇⣿⣿ ",
-	--                  "  'llllll      .:::::lloddd'     ⣿⡇⢀⡲⠄⠄⠈⠙⠻⢿⣿⣿⠿⠿⠟⠛⠋⠁⣰⠇ ⢸⣿⣿⣿ ",
-	--                  "  .looool       .;::coooodo.     ⣿⡇⢬⡻⡇⡄⠄⠄⠄⡰⢖⠔⠉⠄⠄⠄⠄⣼⠏  ⢸⣿⣿⣿ ",
-	--                  "    .cool         'ccoooc.       ⣿⡇⠄⠙⢌⢷⣆⡀⡾⡣⠃⠄⠄⠄⠄⠄⣼⡟    ⢿⣿⣿ ",
-	--                  "      .co          .:o:.        ⠀⣿⡇⡏⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⢀⣠⢶⡇⠀⠀ ⠀⠀⢿⣿ ",
-	--                  "        .           .'           ⠇⠀  ⠀⠀⡿⢣⣀⣀⣀⡤⠴⡞⠉⠀⢸⠀    ⠀⠀  ",
-	-- }
+	vim.g.dashboard_preview_file_height = 14
+	vim.g.dashboard_preview_file_width = 60
 end
 
 return M
