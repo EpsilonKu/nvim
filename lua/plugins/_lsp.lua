64d305f55af29d52679d730ff993fbb19c335af3
DumbEpsKu <63652620+EpsilonKu@users.noreply.github.com>
2021-05-27 04:04:03 +0600

    Removed unnecessary LSP config

--- lua/plugins/_lsp.lua
+++ lua/plugins/_lsp.lua
@@ -4,61 +4,21 @@ local M = {}
 function M.config()
 	local g = vim.g
 	require'lspconfig'.clangd.setup{}
--- 	local nvim_lsp = require('lspconfig')
--- 	local on_attach = function(client, bufnr)
--- 	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
--- 	local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
--- 
--- 	buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
--- 
--- 	-- Mappings.
--- 	local opts = { noremap=true, silent=true }
--- 	buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
--- 	buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
--- 	buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
--- 	buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
--- 	buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
--- 	buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
--- 	buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
--- 	buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
--- 	buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
--- 	buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
--- 	buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
--- 	buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
--- 	buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
--- 	buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
--- 	buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
--- 	buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
--- 
---   -- Set some keybinds conditional on server capabilities
---   if client.resolved_capabilities.document_formatting then
---     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
---   end
---   if client.resolved_capabilities.document_range_formatting then
---     buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
---   end
--- 
---   -- Set autocommands conditional on server_capabilities
---   if client.resolved_capabilities.document_highlight then
---     vim.api.nvim_exec([[
---       hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
---       hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
---       hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
---       augroup lsp_document_highlight
---         autocmd! * <buffer>
---         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
---         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
---       augroup END
---     ]], false)
---   end
--- end
--- 
--- -- Use a loop to conveniently both setup defined servers 
--- -- and map buffer local keybindings when the language server attaches
--- local servers = { "pyright", "rust_analyzer", "tsserver" }
--- for _, lsp in ipairs(servers) do
---   nvim_lsp[lsp].setup { on_attach = on_attach }
--- end
+	-- require'lspconfig'.jdtls.setup{}
+	-- export JAR=/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_1.6.100.v20201223-0822.jar
+	-- export GRADLE_HOME=$HOME/gradle
+	-- export JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
+	-- export JDTLS_CONFIG=/usr/share/java/jdtls/config_linux
+	-- export WORKSPACE=$HOME/Projects/abay-academy/
+	
+end
+
+function M.start_jdtls()
+	-- find_root looks for parent directories relative to the current buffer containing one of the given arguments.
+	require('jdtls').start_or_attach({
+		cmd = {'java-lsp.sh'}, 
+		root_dir = require('jdtls.setup').find_root({'gradle.build', 'pom.xml'})
+	})
 end
 
 return M
