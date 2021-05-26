ea256c350d74529e1318181498828f36014b0c1b
DumbEpsKu <63652620+EpsilonKu@users.noreply.github.com>
2021-05-27 04:06:40 +0600

    Fix LSP colorscheme problem
    Also normal Packer init

--- init.lua
+++ init.lua
@@ -5,7 +5,21 @@ local o = vim.o
 -- This is a magic line that will take your pain away.
 o.rtp = string.format("%s/neovim,%s", D, o.rtp)
 
+-- {{ Packer Init
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+-- }}
+
+vim.cmd 'colorscheme melange'
+
 -- Vanilla Config
-require "plugins"
 require "settings"
+require "plugins"
 
