2e130f07a865a1d80f9c7046869ebdfcf2a0da9c
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2021-07-08 14:37:15 +0600

    Fix Packer Ignore Type

--- lua/plugins/_indent.lua
+++ lua/plugins/_indent.lua
@@ -8,7 +8,8 @@ vim.g.indent_blankline_filetype_exclude = {'help', 'md'}
 vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_context_patterns = {'class', 'function', 'method', 'if',  'else', 'for', 'while', 'public', 'private'}
--- vim.g.indent_blankline_char_highlight_list = {'Error', 'Function'}
+vim.g.indent_blankline_filetype_exclude = {'help', 'chadtree', 'startify', 'fzf', 'tagbar', 'dashboard', 'packer'}
+vim.g.indent_blankline_buftype_exclude = {'terminal'}
 end
 
 return M
