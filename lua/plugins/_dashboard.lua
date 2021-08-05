2e9422cef7182d48316f7dff63ac5999ed9aa45c
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2021-08-05 18:24:23 +0600

    Changing dashboard background

--- lua/plugins/_dashboard.lua
+++ lua/plugins/_dashboard.lua
@@ -6,9 +6,9 @@ function M.config()
 	vim.g.dashboard_default_executive = 'telescope'
 	vim.g.dashboard_preview_command = 'cat'
 	vim.g.dashboard_preview_pipeline = 'lolcat --spread=2.5 -t --seed=156'
-	vim.g.dashboard_preview_file = '~/.config/nvim/etc/logo/girl.cat'
-	vim.g.dashboard_preview_file_height = 14
-	vim.g.dashboard_preview_file_width = 60
+	vim.g.dashboard_preview_file = '~/.config/nvim/etc/logo/sus.cat'
+	vim.g.dashboard_preview_file_height = 21
+	vim.g.dashboard_preview_file_width = 30
 
 	vim.g.dashboard_custom_shortcut={
 		last_session       = 'SPC s l',
