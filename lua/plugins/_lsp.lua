4a2a0ee9b779545a9a103eda0473836635658958
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2021-08-08 22:49:23 +0600

    Configure LSP symbols

--- lua/plugins/_lsp.lua
+++ lua/plugins/_lsp.lua
@@ -3,6 +3,65 @@ local M = {}
 
 function M.config()
 	local g = vim.g
+	
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.fn.sign_define(
+  "LspDiagnosticsSignError",
+  { text = "", numhl = "LspDiagnosticsDefaultError" }
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignWarning",
+  { text = "", numhl = "LspDiagnosticsDefaultWarning" }
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignInformation",
+  { text = "", numhl = "LspDiagnosticsDefaultInformation" }
+)
+vim.fn.sign_define(
+  "LspDiagnosticsSignHint",
+  { text = "", numhl = "LspDiagnosticsDefaultHint" }
+)
+
+-- set default prefix.
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
+    -- virtual_text = false,
+    virtual_text = { prefix = "" },
+    signs = true,
+    update_in_insert = false,
+  }
+)
+	vim.lsp.protocol.CompletionItemKind = {
+  "   (Text) ",
+  "   (Method)",
+  "   (Function)",
+  "   (Constructor)",
+  " ﴲ  (Field)",
+  "[] (Variable)",
+  "   (Class)",
+  " ﰮ  (Interface)",
+  "   (Module)",
+  " 襁 (Property)",
+  "   (Unit)",
+  "   (Value)",
+  " 練 (Enum)",
+  "   (Keyword)",
+  "   (Snippet)",
+  "   (Color)",
+  "   (File)",
+  "   (Reference)",
+  "   (Folder)",
+  "   (EnumMember)",
+  " ﲀ  (Constant)",
+  " ﳤ  (Struct)",
+  "   (Event)",
+  "   (Operator)",
+  "   (TypeParameter)",
+}
+
 	-- require'lspconfig'.clangd.setup{}
 		-- require('jdtls').start_or_attach({
 		-- cmd = {'java-lsp.sh'}, 
