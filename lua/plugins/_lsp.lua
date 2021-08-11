b50878f3de98a2e329232469266d81bc24f4cd08
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2021-08-11 12:53:08 +0600

    Add html lsp

--- lua/plugins/_lsp.lua
+++ lua/plugins/_lsp.lua
@@ -60,7 +60,17 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   "   (Event)",
   "   (Operator)",
   "   (TypeParameter)",
+		}
+--Html lsp
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
 }
+
+
 
 	-- require'lspconfig'.clangd.setup{}
 		-- require('jdtls').start_or_attach({
