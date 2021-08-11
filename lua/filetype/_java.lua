957ad6ef52016e70e247f1f2d5d7a02eb92d1a10
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2021-08-11 12:51:37 +0600

    Enable to debug and decompile
    Also added keybinding for code action

--- lua/filetype/_java.lua
+++ lua/filetype/_java.lua
@@ -14,7 +14,7 @@ end
 
 local function jdtls_on_attach()
 	require('jdtls.setup').add_commands ()
-	require('jdtls').setup_dap({hotcodereplace = 'auto'})
+	require('jdtls').setup_dap()
 	require('jdtls.dap').setup_dap_main_class_configs()
 end
 
@@ -23,12 +23,21 @@ local function jdtls_start()
 	local config = {}
 	local home = os.getenv('HOME')
 	local workspace_folder = home .. "/.workspace/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
-
-	config.init_options = {
-		bundles = {
-			vim.fn.glob("~/.local/bin/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar")
+	local bundles = {
+			vim.fn.glob("~/.local/bin/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"),
 		}
-	}
+
+	vim.list_extend(bundles, vim.split(vim.fn.glob(home .. "/.local/bin/vscode-java-decompiler/server/*.jar"), "\n"))
+	config.init_options = {
+		bundles = bundles,
+		settings = {
+			java = {
+      signatureHelp = { enabled = true };
+      contentProvider = { preferred = 'fernflower' };
+    }
+
+		}
+		}
 	config.cmd = {'jdtls.sh', workspace}
 	config.root_dir = require('jdtls.setup').find_root({'gradle.build', 'pom.xml'})
 	config.on_attach = jdtls_on_attach
@@ -43,11 +52,15 @@ function M.init()
 			name = "+Java",
 			b = { "<CMD>lua require('filetype._java').build()<CR>", "  Java Build Spring" },
 			d = { ":lua require'dap'.continue()<CR>", "  Java Debug Run"}
+		},
+		["<leader>c"] = {
+			name = "+Code Action",
+			c = {":lua require('jdtls').code_action()<CR>", " Code action"},
+			r = {":lua require('jdtls').code_action(false, 'refactor')<CR>", " Code refactor"}
 		}
 	})
 
-
--- 	command! -buffer JdtCompile lua require('jdtls').compile()
+--	command! -buffer JdtCompile lua require('jdtls').compile()
 -- command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()
 -- command! -buffer JdtJol lua require('jdtls').jol()
 -- command! -buffer JdtBytecode lua require('jdtls').javap()
