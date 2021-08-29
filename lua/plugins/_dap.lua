71baa3152e6c000a126bce655983d6a7d8807124
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2021-08-29 23:20:37 +0600

    Lsp new config

--- lua/plugins/_dap.lua
+++ lua/plugins/_dap.lua
@@ -1,104 +1,69 @@
-
 local M = {}
 
 function M.config()
-	local U = require "utils"
-	local wk = require("which-key")
-	local dap = require ("dap")
+    local U = require "utils"
+    local wk = require("which-key")
+    local dap = require("dap")
 
-	require("dapui").setup({
-		icons = {
-			expanded = "↴",
-			collapsed = "»"
-		},
-	mappings = {
-    -- Use a table to apply multiple mappings
-		expand = {"<CR>", "<2-LeftMouse>"},
-		open = "o",
-		remove = "d",
-		edit = "e",
-		repl = "r",
-	},
-	sidebar = {
-    open_on_start = false,
-    elements = {
-		 {
-        id = "scopes",
-        size = 0.25, -- Can be float or integer > 1
-      },
-      { id = "breakpoints", size = 0.25 },
-      { id = "stacks", size = 0.25 },
-      { id = "watches", size = 0.25 },
-      -- You can change the order of elements in the sidebar
-    },
-    width = 40,
-    position = "left" -- Can be "left" or "right"
-  },
-	tray = {
-		open_on_start = true,
-		elements = {
-				{id = "scopes", size= 0.4},
-				{id = "stacks", size= 0.6}
-		},
-		height = 10,
-		position = "bottom" -- Can be "bottom" or "top"
-	},
-	floating = {
-		max_height = 0.4, -- These can be integers or a float between 0 and 1.
-		max_width = 0.3,   -- Floats will be treated as percentage of your screen.
-		mappings = {
-    		close = { "q", "<Esc>" },
-    	},
-	}
-	})
+    require("dapui").setup(
+        {
+            icons = {
+                expanded = "↴",
+                collapsed = "»"
+            },
+            mappings = {
+                -- Use a table to apply multiple mappings
+                expand = {"<CR>", "<2-LeftMouse>"},
+                open = "o",
+                remove = "d",
+                edit = "e",
+                repl = "r"
+            },
+            sidebar = {
+                open_on_start = false,
+                elements = {
+                    {
+                        id = "scopes",
+                        size = 0.25 -- Can be float or integer > 1
+                    },
+                    {id = "breakpoints", size = 0.25},
+                    {id = "stacks", size = 0.25},
+                    {id = "watches", size = 0.25}
+                    -- You can change the order of elements in the sidebar
+                },
+                width = 40,
+                position = "left" -- Can be "left" or "right"
+            },
+            tray = {
+                open_on_start = true,
+                elements = {
+                    {id = "scopes", size = 0.4},
+                    {id = "stacks", size = 0.6}
+                },
+                height = 10,
+                position = "bottom" -- Can be "bottom" or "top"
+            },
+            floating = {
+                max_height = 0.4, -- These can be integers or a float between 0 and 1.
+                max_width = 0.3 -- Floats will be treated as percentage of your screen.
+            }
+        }
+    )
 
-	require("dapui").float_element("watches")
+    require("dapui").float_element("watches")
 
-	vim.fn.sign_define('DapBreakpoint', {text='', texthl='', linehl='', numhl=''})
--- vim.fn.sign_define('DapLogPoint', {text='', texthl='', linehl='', numhl=''})
--- vim.fn.sign_define('DapStopped', {text='', texthl='', linehl='', numhl=''})
-	
+    vim.fn.sign_define("DapBreakpoint", {text = "", texthl = "", linehl = "", numhl = ""})
+    vim.fn.sign_define("DapLogPoint", {text = "", texthl = "", linehl = "", numhl = ""})
+    vim.fn.sign_define("DapStopped", {text = "", texthl = "", linehl = "", numhl = ""})
 
-	wk.register({
-		["<leader>d"] = {
-			name = "+Debugger",
-			a = { ":lua require'dap'.toggle_breakpoint()<CR>", "  Toggle Breakpoint" },
-			s = { ":lua require'dap'.step_over()<CR>", "  Debug Step Over" },
-			d = { ":lua require'dap'.step_into()<CR>", "  Debug Step Into" },
-			f = { ":lua require'dap'.step_out()<CR>", "  Debug Step Out" },
-			e = { ":lua require'dap'.continue()<CR>", "  Debug Continue" },
-			q = { ":lua require'dap'.close()<CR>", " 栗Debug Stop Process" },
-		},
-		["<C-d>"] = {
-			name = "+Debugger quick actions",
-			f = { ':Telescope dap frames<CR>', " ﮆ Frames" },
-			b = { ':Telescope dap list_breakpoints<CR>', " ךּ List breakpoints" },
-			v = { ':Telescope dap variables<CR>', "  Variables" },
-			c = { ':Telescope dap commands<CR>', " גּ Debug commands" },
-			i = { ':lua require("dapui").float_element("stacks")<CR>', "  Variables hover" },
-		}
-	})
+    vim.g.dap_virtual_text = true
 
-	vim.g.dap_virtual_text = true
-
-	dap.defaults.fallback.external_terminal = {
-    command = '/usr/bin/kitty';
-		args = {'-e'};
-	}
-	  dap.defaults.fallback.force_external_terminal = true
-	-- dap.defaults.fallback.terminal_win_cmd = ":lua require('FTerm').open_empty()"
-
-    -- nnoremap <silent> <f5> :lua require'dap'.continue()<cr>
-    -- nnoremap <silent> <f10> :lua require'dap'.step_over()<cr>
-    -- nnoremap <silent> <f11> :lua require'dap'.step_into()<cr>
-    -- nnoremap <silent> <f12> :lua require'dap'.step_out()<cr>
-    -- nnoremap <silent> <leader>b :lua require'dap'.toggle_breakpoint()<cr>
-    -- nnoremap <silent> <leader>b :lua require'dap'.set_breakpoint(vim.fn.input('breakpoint condition: '))<cr>
-    -- nnoremap <silent> <leader>lp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('log point message: '))<cr>
-    -- nnoremap <silent> <leader>dr :lua require'dap'.repl.open()<cr>
-    -- nnoremap <silent> <leader>dl :lua require'dap'.run_last()<CR>
--- dap.ui.variables.hover()
+    dap.defaults.fallback.external_terminal = {
+        command = "/usr/bin/kitty",
+        args = {"-e"}
+    }
+    dap.defaults.fallback.force_external_terminal = true
+    -- dap.defaults.fallback.terminal_win_cmd = ":lua require('FTerm').open_empty()"
 end
 
 return M
-
--- lua/plugins/_lsp.lua
+++ lua/plugins/_lsp.lua
@@ -1,92 +1,93 @@
-
 local M = {}
 
 function M.config()
-	local g = vim.g
-	
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
+    local g = vim.g
 
-vim.fn.sign_define(
-  "LspDiagnosticsSignError",
-  { text = "", numhl = "LspDiagnosticsDefaultError" }
-)
-vim.fn.sign_define(
-  "LspDiagnosticsSignWarning",
-  { text = "", numhl = "LspDiagnosticsDefaultWarning" }
-)
-vim.fn.sign_define(
-  "LspDiagnosticsSignInformation",
-  { text = "", numhl = "LspDiagnosticsDefaultInformation" }
-)
-vim.fn.sign_define(
-  "LspDiagnosticsSignHint",
-  { text = "", numhl = "LspDiagnosticsDefaultHint" }
-)
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
 
--- set default prefix.
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics,
-  {
-    -- virtual_text = false,
-    virtual_text = { prefix = "" },
-    signs = true,
-    update_in_insert = false,
-  }
-)
-	vim.lsp.protocol.CompletionItemKind = {
-  "   (Text) ",
-  "   (Method)",
-  "   (Function)",
-  "   (Constructor)",
-  " ﴲ  (Field)",
-  "[] (Variable)",
-  "   (Class)",
-  " ﰮ  (Interface)",
-  "   (Module)",
-  " 襁 (Property)",
-  "   (Unit)",
-  "   (Value)",
-  " 練 (Enum)",
-  "   (Keyword)",
-  "   (Snippet)",
-  "   (Color)",
-  "   (File)",
-  "   (Reference)",
-  "   (Folder)",
-  "   (EnumMember)",
-  " ﲀ  (Constant)",
-  " ﳤ  (Struct)",
-  "   (Event)",
-  "   (Operator)",
-  "   (TypeParameter)",
-		}
---Html lsp
---Enable (broadcasting) snippet capability for completion
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+    vim.fn.sign_define("LspDiagnosticsSignError", {text = "", numhl = "LspDiagnosticsDefaultError"})
+    vim.fn.sign_define("LspDiagnosticsSignWarning", {text = "", numhl = "LspDiagnosticsDefaultWarning"})
+    vim.fn.sign_define("LspDiagnosticsSignInformation", {text = "", numhl = "LspDiagnosticsDefaultInformation"})
+    vim.fn.sign_define("LspDiagnosticsSignHint", {text = "", numhl = "LspDiagnosticsDefaultHint"})
 
-require'lspconfig'.html.setup {
-  capabilities = capabilities,
-}
+    -- set default prefix.
+    vim.lsp.handlers["textDocument/publishDiagnostics"] =
+        vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics,
+        {
+            -- virtual_text = false,
+            virtual_text = {prefix = ""},
+            signs = true,
+            update_in_insert = false
+        }
+    )
+    vim.g.nvim_tree_update_cwd = 1
+    vim.g.nvim_tree_respect_buf_cwd = 1
+    require("project_nvim").setup {
+        manual_mode = false,
+        -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+        -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+        -- order matters: if one is not detected, the other is used as fallback. You
+        -- can also delete or rearangne the detection methods.
+        detection_methods = {"lsp", "pattern"},
+        -- All the patterns used to detect root dir, when **"pattern"** is in
+        -- detection_methods
+        patterns = {".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "pom.xml"},
+        -- Table of lsp clients to ignore by name
+        -- eg: { "efm", ... }
+        ignore_lsp = {},
+        -- Show hidden files in telescope
+        show_hidden = false,
+        -- When set to false, you will get a message when project.nvim changes your
+        -- directory.
+        silent_chdir = true,
+        -- Path where project.nvim will store the project history for use in
+        -- telescope
+        datapath = vim.fn.stdpath("data")
+    }
+    require("lspkind").init(
+        {
+            with_text = true,
+            preset = "codicons",
+            symbol_map = {
+                Text = "",
+                Method = "",
+                Function = "",
+                Constructor = "",
+                Field = "ﰠ",
+                Variable = "",
+                Class = "ﴯ",
+                Interface = "",
+                Module = "",
+                Property = "ﰠ",
+                Unit = "塞",
+                Value = "",
+                Enum = "",
+                Keyword = "",
+                Snippet = "",
+                Color = "",
+                File = "",
+                Reference = "",
+                Folder = "",
+                EnumMember = "",
+                Constant = "",
+                Struct = "פּ",
+                Event = "",
+                Operator = "",
+                TypeParameter = ""
+            }
+        }
+    )
 
+    --Html lsp
+    --Enable (broadcasting) snippet capability for completion
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-	-- require'lspconfig'.clangd.setup{}
-		-- require('jdtls').start_or_attach({
-		-- cmd = {'java-lsp.sh'}, 
-		-- root_dir = require('jdtls.setup').find_root({'gradle.build', 'pom.xml'})
-		-- })
-	-- require'lspconfig'.java_language_server.setup{
-	-- 	cmd = {"java-language-server"}
-	-- }
-	-- require'lspconfig'.jdtls.setup{}
-	-- export JAR=/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_1.6.100.v20201223-0822.jar
-	-- export GRADLE_HOME=$HOME/gradle
-	-- export JAVA_HOME=/usr/lib/jvm/java-11-openjdk/
-	-- export JDTLS_CONFIG=/usr/share/java/jdtls/config_linux
-	-- export WORKSPACE=$HOME/Projects/abay-academy/
-	
+    require "lspconfig".html.setup {
+        capabilities = capabilities
+    }
 end
 
 return M
-
