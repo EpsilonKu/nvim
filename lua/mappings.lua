0b97ae2385bfa086f38e35bf9293ca1e1dfac7a2
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2022-02-02 20:54:43 +0600

    Fix window navigation

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,10 +5,10 @@ local U = require "utils"
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', "<A-h>", "<CMD>lua require('Navigator').left()<CR>", opts)
-map('n', "<A-k>", "<CMD>lua require('Navigator').up()<CR>", opts)
-map('n', "<A-l>", "<CMD>lua require('Navigator').right()<CR>", opts)
-map('n', "<A-j>", "<CMD>lua require('Navigator').down()<CR>", opts)
+map('n', "<A-j>", "<CMD>lua require('Navigator').left()<CR>", opts)
+map('n', "<A-l>", "<CMD>lua require('Navigator').up()<CR>", opts)
+map('n', "<A-;>", "<CMD>lua require('Navigator').right()<CR>", opts)
+map('n', "<A-k>", "<CMD>lua require('Navigator').down()<CR>", opts)
 map('n', "<A-p>", "<CMD>lua require('Navigator').previous()<CR>", opts)
 
 U.map("v", "c", "d")
