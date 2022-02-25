8c3953664783aff9571910af029a3ab70ab518ef
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2022-02-25 10:07:42 +0600

    Config which-key

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,8 +1,29 @@
-	 require("which-key").setup {
-		icons = {
-			breadcrumb = "", -- symbol used in the command line area that shows your active key combo
-			separator = "", -- symbol used between a key and it's label
-			group = "祐", -- symbol prepended to a group
-		},
-	}
+require("which-key").setup {
+    icons = {
+        breadcrumb = "", -- symbol used in the command line area that shows your active key combo
+        separator = "", -- symbol used between a key and it's label
+        group = "祐" -- symbol prepended to a group
+    },
+plugins = {
+    marks = true, -- shows a list of your marks on ' and `
+    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    spelling = {
+      enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      suggestions = 20, -- how many suggestions should be shown in the list?
+    },
+    -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+    -- No actual key bindings are created
+    presets = {
+      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+      motions = false, -- adds help for motions
+      text_objects = false, -- help for text objects triggered after entering an operator
+      windows = true, -- default bindings on <c-w>
+      nav = false, -- misc bindings to work with windows
+      z = false, -- bindings for folds, spelling and others prefixed with z
+      g = true, -- bindings for prefixed with g
+    },
+  },
+  -- add operators that will trigger motion and text object completion
+  -- to enable all native operators, set the preset / operators plugin above
+}
 
